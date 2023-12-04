60f57a4d3b36e51dc65bb414cbc3dedf1a4e5518
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-12-04 14:51:32 +0200

    +++

---
+++ digital-business-card/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import About from "./components/About";
+import Footer from "./components/Footer";
+
+import Info from "./components/Info";
+import Interests from "./components/Interests";
+
+function App() {
+  return (
+    <div className="container">
+      <Info />
+      <About />
+      <Interests />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ digital-business-card/src/components/Info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Info = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <h1>Vugar Gasimov</h1>
+      <p>Frontend Developer</p>
+      <p>vugargasimovth.website</p>
+      <a href="">Email</a>
+      <a href="">LinkedIn</a>
+    </div>
+  );
+};
+
+export default Info;
