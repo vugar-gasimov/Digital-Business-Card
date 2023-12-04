c7ac75f55a5b25f434ed6c28b2260dd3baec5eeb
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-12-04 20:28:40 +0200

    links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,22 +4,28 @@ const Footer = () => {
   return (
     <div className="footer--container">
       <Symbols />
-      <a className="footer--button" href="">
+      <a className="footer--button" href="https://twitter.com/VuqarQasim46049">
         <svg className="icon" width={25} height={25}>
           <use xlinkHref="#icon-Twitter" />
         </svg>
       </a>
-      <a className="footer--button" href="">
+      <a
+        className="footer--button"
+        href="https://www.facebook.com/profile.php?id=100006303706598"
+      >
         <svg className="icon" width={25} height={25}>
           <use xlinkHref="#icon-Facebook" />
         </svg>
       </a>
-      <a className="footer--button" href="">
+      <a
+        className="footer--button"
+        href="https://instagram.com/_vugargasimov_?igshid=MmVlMjlkMTBhMg=="
+      >
         <svg className="icon" width={25} height={25}>
           <use xlinkHref="#icon-Instagram" />
         </svg>
       </a>
-      <a className="footer--button" href="">
+      <a className="footer--button" href="https://github.com/vugar-gasimov">
         <svg className="icon" width={25} height={25}>
           <use xlinkHref="#icon-GitHub" />
         </svg>
